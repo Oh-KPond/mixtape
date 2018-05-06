9b357f9a08d65379bdb01f9ccde21103a4b1ebe2
Kate Pond <kate@thepondsedge.com>
2018-05-06 11:20:06 -0700

    creates a json to be called on in the controller

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,4 +1,6 @@
 class SongsController < ApplicationController
   def index
+    songs = Song.all.first(5)
+    render :json => songs.as_json(only: [:id, :title, :artist, :year]), status: :ok
   end
 end
