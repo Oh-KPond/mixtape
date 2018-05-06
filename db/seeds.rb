75be4865c8b13885c32dfd9c93a50b128549a9cd
Kate Pond <kate@thepondsedge.com>
2018-05-06 10:58:40 -0700

    creates seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+require 'date'
+
+Song.destroy_all
+
+50.times do |index|
+  Song.create(title: Faker::Cat.name,
+              artist: Faker::Kpop.girl_groups,
+              year: (rand 1900..2018))
+end
+
+p "Created #{Song.count} songs"
